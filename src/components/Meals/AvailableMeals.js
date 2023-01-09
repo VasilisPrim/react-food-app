20e48042a7e986e070abb6d1855efcb77338d320
Bill <bprimikiris@gmail.com>
2023-01-09 17:24:13 +0200

    Fetching data from firebase

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,39 +1,59 @@
-
+import { useState ,useEffect} from 'react';
 import MealItem from './MealItem/MealItem';
 import Card from '../UI/Card';
 import classes from './AvailableMeals.module.css';
 
-const DUMMY_MEALS = [
-    {
-      id: 'm1',
-      name: 'Sushi',
-      description: 'Finest fish and veggies',
-      price: 22.99,
-    },
-    {
-      id: 'm2',
-      name: 'Schnitzel',
-      description: 'A german specialty!',
-      price: 16.5,
-    },
-    {
-      id: 'm3',
-      name: 'Barbecue Burger',
-      description: 'American, raw, meaty',
-      price: 12.99,
-    },
-    {
-      id: 'm4',
-      name: 'Green Bowl',
-      description: 'Healthy...and green...',
-      price: 18.99,
-    },
-  ];
 
 
 
 const AvailableMeals = () =>{
-    const mealsList = DUMMY_MEALS.map((meal) =>{
+
+  
+
+  const [mealData,setMealData] = useState([]);
+
+
+  
+  
+
+  
+
+
+  useEffect(() => {
+   const getMeals = async () => {
+     
+       const response = await fetch(
+         "https://react-http-29822-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
+       );
+
+
+       const data = await response.json();
+
+       const mealArray = [];
+
+       for (const key in data) {
+         mealArray.push({
+          id:key,
+          name:data[key].name,
+          description:data[key].description,
+          price:data[key].price
+
+         });
+       }
+
+
+       setMealData(mealArray);
+     } 
+     getMeals();
+     
+   
+  }, []);
+
+
+
+
+    const mealsList = mealData.map((meal) =>{
+        
         return <MealItem id = {meal.id} key={meal.id} name={meal.name} description = {meal.description} price = {meal.price}/>
         })
 
