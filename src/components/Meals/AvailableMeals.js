065a5340b80a482b80fceedc2b6b598f166f0183
Bill <bprimikiris@gmail.com>
2023-01-10 12:38:19 +0200

    Add error handling and loading state

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,70 +1,91 @@
-import { useState ,useEffect} from 'react';
-import MealItem from './MealItem/MealItem';
-import Card from '../UI/Card';
-import classes from './AvailableMeals.module.css';
+import { useState, useEffect } from "react";
+import MealItem from "./MealItem/MealItem";
+import Card from "../UI/Card";
+import classes from "./AvailableMeals.module.css";
 
+const AvailableMeals = () => {
+  const [mealData, setMealData] = useState([]);
 
+  const [error, setError] = useState();
+ 
 
-
-const AvailableMeals = () =>{
-
-  
-
-  const [mealData,setMealData] = useState([]);
-
-
-  
-  
-
-  
-
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-   const getMeals = async () => {
-     
-       const response = await fetch(
-         "https://react-http-29822-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
-       );
+    const getMeals = async () => {
+      
+      
+        const response = await fetch(
+          "https://react-http-29822-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
+        );
 
+        if(!response.ok){
+          throw new Error('There was an error in fetching data.Check your connection!')
+        }
 
-       const data = await response.json();
+        const data = await response.json();
+      
+      
 
-       const mealArray = [];
+      const mealArray = [];
 
-       for (const key in data) {
-         mealArray.push({
-          id:key,
-          name:data[key].name,
-          description:data[key].description,
-          price:data[key].price
+      for (const key in data) {
+        mealArray.push({
+          id: key,
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price,
+        });
+      }
 
-         });
-       }
+      setMealData(mealArray);
+      setIsLoading(false);
+    };
 
-
-       setMealData(mealArray);
-     } 
-     getMeals();
-     
-   
+    
+    getMeals().catch(error =>{
+      setIsLoading(false);
+      setError(error.message)
+    });
   }, []);
 
+  
 
+  if (isLoading) {
+    return (
+      <section className={classes.loading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
 
+  if (error) {
+    return (
+      <section className={classes.error}>
+        <p>{error}</p>
+      </section>
+    );
+  }
 
-    const mealsList = mealData.map((meal) =>{
-        
-        return <MealItem id = {meal.id} key={meal.id} name={meal.name} description = {meal.description} price = {meal.price}/>
-        })
+  const mealsList = mealData.map((meal) => {
+    return (
+      <MealItem
+        id={meal.id}
+        key={meal.id}
+        name={meal.name}
+        description={meal.description}
+        price={meal.price}
+      />
+    );
+  });
 
-    return <section className={classes.meals}>
-        <Card>
-
+  return (
+    <section className={classes.meals}>
+      <Card>
         <ul>{mealsList}</ul>
-        </Card>
-        </section>
-            
-}
-
+      </Card>
+    </section>
+  );
+};
 
 export default AvailableMeals;
